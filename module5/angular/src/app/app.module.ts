b7de7984be33ed73c23e7f72454e4ebc5b34dd31
Hoangtu9999 <58470170+Hoangtu9999@users.noreply.github.com>
2020-12-25 09:08:13 +0700

    upbaimoi

--- module5/angular/src/app/app.module.ts
+++ module5/angular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponent } from './_03_angular_overview/thuc_hanh/test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
